6e99256d640ea5371914602d309ae10be931d25d
Andrés Felipe Alvarez R <andresfelipe.alvarez03@gmail.com>
2021-10-08 19:28:57 -0500

    sidebar modified

--- bibliotech/src/components/barraNav.vue
+++ bibliotech/src/components/barraNav.vue
@@ -29,71 +29,41 @@
 
     <!-- Grupo de elementos #2 -->
     <div class=" contenedor-body-1">
-      <!-- Primer elemento de la barra lateral -->
-      <div class="contenedor-barra-1" onclick="javascript:enlaceRecientes();">
-        <div class="elemento-1">
-          <div class="elemento-1__icono">
-            <span class="material-icons material-icons--settings">restore</span>
-          </div>
-          <p class="elemento-1__titulo mb-0">Recientes</p>
-        </div>
-      </div>
+      <newerBar />
 
-      <!-- Segundo elemento de la barra lateral -->
-      <div
-        class="contenedor-barra-2 contenedor-barra-1--actual"
-        onclick="javascript:enlaceArticulos();"
-      >
-        <div class="elemento-1">
-          <div class="elemento-1__icono">
-            <span class="material-icons material-icons--settings-actual"
-              >import_contacts</span
-            >
-          </div>
-          <p class="mb-0 elemento-1__titulo elemento-1__titulo--actual">
-            Artículos
-          </p>
-        </div>
-      </div>
+      <itemBar />
 
-      <!-- Tercer elemento de la barra lateral -->
-      <div
-        class="contenedor-barra-3"
-        onclick="javascript:enlaceAdministradores();"
-      >
-        <div class="elemento-1">
-          <div class="elemento-1__icono">
-            <span class="material-icons material-icons--settings">group</span>
-          </div>
-          <p class="mb-0 elemento-1__titulo">Administradores</p>
-        </div>
-      </div>
+      <adminBar />
+
     </div>
     <!-- FIN Grupo de elementos #2 -->
 
     <!-- Grupo de elementos #3 -->
     <div class="contenedor-body-2">
-      <!-- Tercer elemento de la barra lateral -->
-      <div class="contenedor-barra-1" onclick="javascript:enlaceSalir();">
-        <div class="elemento-1">
-          <div class="elemento-1__icono">
-            <span class="material-icons material-icons--settings">logout</span>
-          </div>
-          <p class="mb-0 elemento-1__titulo">Salir</p>
-        </div>
-      </div>
+      <logoutBar />
     </div>
     <!-- FIN Grupo de elementos #3 -->
   </div>
 </template>
 
 <script>
-import labelRoot from "../components/labelRoot.vue";
-import labelAdmin from "../components/labelAdmin.vue";
+// Labels //
+import labelRoot from "../components/labelRoot.vue"
+import labelAdmin from "../components/labelAdmin.vue"
+// Elementos de la barra de navegación //
+import newerBar from "../components/sideBar/newerBar.vue"
+import itemBar from "../components/sideBar/itemBar.vue"
+import adminBar from "../components/sideBar/adminBar.vue"
+import logoutBar from "../components/sideBar/logoutBar.vue"
+
 export default {
   components: {
     labelRoot,
     labelAdmin,
+    newerBar,
+    itemBar,
+    adminBar,
+    logoutBar
   },
 };
 </script>
@@ -156,54 +126,6 @@ export default {
   box-sizing: border-box;
 }
 
-/* Primera Sección */
-.contenedor-barra-1 {
-  /* background-color: crimson;  WIREFRAME*/
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-
-  width: 100%;
-  height: 2.5rem;
-
-  padding-left: 0.625rem;
-  padding-right: 0.625rem;
-
-  box-sizing: border-box;
-
-  /*border-radius: 4px;*/
-}
-
-/* Primera Sección - elemento*/
-.elemento-1 {
-  /* background-color: rgb(255, 0, 242); WIREFRAME*/
-
-  padding: 0;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 1.25rem;
-}
-
-/* Primera Sección - icono*/
-.elemento-1__icono {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-content: center;
-
-  margin-right: 1.5rem;
-}
-
-.elemento-1__titulo {
-  color: #828282;
-  font-family: "Roboto", sans-serif;
-  font-size: 1.1rem;
-  font-weight: 300;
-  letter-spacing: 0.2px;
-}
-
 /* Borde gris al pasar el mouse por encima del menú*/
 .contenedor-barra-1:hover {
   background-color: #00000009;
@@ -211,11 +133,7 @@ export default {
   cursor: pointer;
 }
 
-.contenedor-barra-2:hover {
-  background-color: #00000009;
-  border-radius: 4px;
-  cursor: pointer;
-}
+
 
 .contenedor-barra-3:hover {
   background-color: #00000009;
@@ -242,39 +160,6 @@ export default {
 
 /* <-----------------------------------> */
 
-.contenedor-barra-2 {
-  /* background-color: yellowgreen;  WIREFRAME*/
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  width: 100%;
-  height: 2.5rem;
-
-  padding-left: 0.625rem;
-  padding-right: 0.625rem;
-
-  box-sizing: border-box;
-
-  /*border-radius: 4px;*/
-}
-
-.contenedor-barra-3 {
-  /* background-color: burlywood;  WIREFRAME*/
-
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  width: 100%;
-  height: 2.5rem;
-
-  padding-left: 0.625rem;
-  padding-right: 0.625rem;
-
-  box-sizing: border-box;
-
-  /*border-radius: 4px;*/
-}
 
 /* Segunda sección del menú */
 
